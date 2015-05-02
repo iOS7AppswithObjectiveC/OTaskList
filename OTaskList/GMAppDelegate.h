928c80a44ffc7b609d52766e7367c163a4164cdc
Ed McCormic <edmccormic@gmail.com>
2015-05-01 21:11:38 -0600

    Initial Commit

---
+++ OTaskList/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  OTaskList
+//
+//  Created by ED on 5/1/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OTaskList/GMViewController.m
@@ -0,0 +1,29 @@
+//
+//  GMViewController.m
+//  OTaskList
+//
+//  Created by ED on 5/1/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import "GMViewController.h"
+
+@interface GMViewController ()
+
+@end
+
+@implementation GMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
